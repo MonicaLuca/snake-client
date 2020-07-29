396afb04f3f099b9a08680171437c9a613d98a7a
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-29 13:05:20 -0400

    input.js movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,6 +14,22 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    // console.log('up');
+    connection.write('Move: up');
+  }
+  if (key === 's') {
+    // console.log('down');
+    connection.write('Move: down');
+  }
+  if (key === 'a') {
+    // console.log('left');
+    connection.write('Move: left');
+  }
+  if (key === 'd') {
+    // console.log('right');
+    connection.write('Move: right');
   }
 };
 
